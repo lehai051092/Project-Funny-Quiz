221992b2eb7bc936848f6098d96046e6245a089d
lehai051092 <mskumji5@gmail.com>
2019-12-04 16:08:10 +0700

    href loginm logout register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,12 +68,33 @@
     <aside id="fh5co-aside" role="complementary" class="border js-fullheight">
 
         <h1 id="fh5co-logo"><a href="">FUNNY QUIZ</a></h1>
+
         <nav id="fh5co-main-menu" role="navigation">
             <ul>
-                <li class="fh5co-active"><a href="">Home</a></li>
+                <li class="fh5co-active"><a href="/">Home</a></li>
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li> @if (Route::has('register'))
+                        <li class="nav-item"><a class="nav-link"
+                                                href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li> @endif @else
+                    <li class="nav-item dropdown"><a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                                                     role="button" data-toggle="dropdown" aria-haspopup="true"
+                                                     aria-expanded="false" v-pre> {{ Auth::user()->name }} <span
+                                class="caret"></span> </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown"><a
+                                class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
+                            <a class="dropdown-item" href="#">Information</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;"> @csrf </form>
+                        </div>
+                    </li> @endguest
                 <li><a href="{{route('categories.list')}}">Blog</a></li>
                 <li><a href="about.html">About</a></li>
                 <li><a href="contact.html">Contact</a></li>
+
             </ul>
         </nav>
 
